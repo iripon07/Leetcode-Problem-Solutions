d2f8699d2f6b5dca9fdc0ebbfcc487a43b146c9a
AsadulIslamRipon <mdasadulripon@gmail.com>
2023-09-19 03:16:07 +0600

    Excel Sheet Column Number

---
+++ String/Easy/171. Excel Sheet Column Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int ans =0;
+        for(auto ch:columnTitle){
+            ans = ans*26 + (ch-'A'+1);
+        }
+        return ans;
+        
+    }
+};
